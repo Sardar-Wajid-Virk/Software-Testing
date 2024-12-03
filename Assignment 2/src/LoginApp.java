ec5cab0ec69b45aa0824490bf8b0b9d1a16ee3bd
Wajid Hassan Virk <wajidvirk777@gmail.com>
2024-12-03 16:41:00 +0500

    Added print statement for testing

--- Assignment 2/src/LoginApp.java
+++ Assignment 2/src/LoginApp.java
@@ -15,6 +15,8 @@ public class LoginApp extends JFrame {
     private static final String DB_PASSWORD = "@Virkisking777";
 
     public LoginApp() {
+
+        System.out.println("LoginApp has started!");
         setTitle("Login Screen");
         setSize(350, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
